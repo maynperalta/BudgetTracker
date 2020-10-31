3431ffff07642ed1eb98640556944ee4c1e13d3e
Maynard Peralta <maynperalta@gmail.com>
2020-10-30 20:02:29 -0600

    added db.js to index.html

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(reg => {
+        console.log("We found your service worker file!", reg);
+      });
+  });
+}
+
 let transactions = [];
 let myChart;
+
+
 
 fetch("/api/transaction")
   .then(response => {
